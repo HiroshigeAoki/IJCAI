a4af27398cf6af3c47e19cdfdf9b65909ea619b1
HiroshigeAoki <a109k9i8@gmail.com>
2022-10-24 19:04:18 +0900

    xai, planningも追加。

--- keyword.py
+++ keyword.py
@@ -5,24 +5,28 @@ import pandas as pd
 from tqdm import tqdm
 import os
 import joblib
+import sys
 
 os.makedirs('./csv/nlp', exist_ok=True)
 
-# Proceedingsの論文でKeywordに"Natural Language Processing"を含むもの。
+# IJCAIのProceedingsの論文から、第一引数で指定されたカテゴリ(nlp, xai, planning)の論文の「タイトル」、「トラック」、「カテゴリ」、「URL」、「概要」を取得する。
+# 実行時間：約8分程度。
 
 years = [2022, 2021, 2020, 2019, 2018, 2017]
+cat = sys.argv[1]
 
 def extract_nlp(year):
     res = requests.get(f'https://www.ijcai.org/proceedings/{year}/')
     soup = BeautifulSoup(res.content, features="html.parser")
     details = soup.find_all('div', {'class': 'details'})
-    titles, tracks, categories, urls = [], [], [], []
+    titles, descs, tracks, categories, urls = [], [], [], [], []
     for detail in tqdm(details, desc=f'{year}'):
         href = detail.find_all('a')[1]['href']
         res = requests.get(f'https://www.ijcai.org{href}')
         url = res.url
         soup = BeautifulSoup(res.content, features="html.parser")
         title = soup.find('h1', {'class', 'page-title'}).text
+        desc = re.sub(r'[\n\t\r]+', ' ', soup.find('div', {'class': 'col-md-12'}).text.strip())
         try:
             track = re.findall(r'(\w+) [tT]rack', soup.find_all('div', {'class': 'col-sm-12'})[1].text)[0]
         except IndexError:
@@ -32,22 +36,33 @@ def extract_nlp(year):
         for topic in topics:
             mcat, *scat = map(lambda x: x.strip(), topic.text.split(': '))
             category.setdefault(mcat, []).append('_'.join(scat))
-        
-        nlp_category = category.get('Natural Language Processing', None)
-        if nlp_category is not None:
+        if cat == 'nlp':
+            category = category.get('Natural Language Processing', None)
+        elif cat == 'xai':
+            category = category.get('AI Ethics, Trust, Fairness', None)
+            if category is None:
+                category = category.get('AI Ethics', None)
+        elif cat == 'planning':
+            category = category.get('Planning and Scheduling', None)
+            
+                
+        if category is not None:
             titles.append(title)
+            descs.append(desc)
             tracks.append(track)
-            categories.append(nlp_category)
+            categories.append(category)
             urls.append(url)
-        
+    
+    os.makedirs(f'./csv/{cat}', exist_ok=True)
     pd.DataFrame(
         dict(
             title=titles,
             track=tracks,
             categories=categories,
-            url=urls,            
+            url=urls,
+            desc=descs        
         )
-    ).to_csv(f'./csv/nlp/{year}.csv')
+    ).to_csv(f'./csv/{cat}/{year}.csv')
 
 
 def main():
--- session.py
+++ session.py
@@ -4,24 +4,29 @@ import re
 import pandas as pd
 from tqdm import tqdm
 import os
+import sys
 
-os.makedirs('./csv/nlp', exist_ok=True)
+cat = sys.argv[1]
 
-# ProceedingsでMain Trackの中の"Natural Language Processing"のところだけ取ったもの。
+# Proceedingsで、第一引数で指定したカテゴリーののsessionに属する論文のみ抽出。
+if cat == 'nlp':
+    year_num = [(2022, 9), (2021, 12), (2020, 11), (2019, 10), (2018, 9), (2017, 7)]
+elif cat == 'xai':
+    year_num = [(2022, 1), (2021, 1)]
 
-year_num = [(2022, 9), (2021, 12), (2020, 11), (2019, 10), (2018, 9), (2017, 7)]
 for year, num in tqdm(year_num):
     res = requests.get(f'https://www.ijcai.org/proceedings/{year}/')
     soup = BeautifulSoup(res.content, features="html.parser")
     nlp = soup.find('div', {'id': f'subsection{num}'})
     papers = nlp.find_all('div', {'class': 'paper_wrapper'})
-    titles, tracks, categories, urls = [], [], [], []
+    titles, descs, tracks, categories, urls = [], [], [], [], []
     for paper in papers:
         href = paper.find('div', {'class': 'details'}).find_all('a')[1]['href']
         res = requests.get(f'https://www.ijcai.org{href}')
         url = res.url
         soup = BeautifulSoup(res.content, features="html.parser")
         title = soup.find('h1', {'class', 'page-title'}).text
+        desc = re.sub(r'[\n\t\r]+', ' ', soup.find('div', {'class': 'col-md-12'}).text.strip())
         track = re.findall(r'(\w+) [tT]rack', soup.find_all('div', {'class': 'col-sm-12'})[1].text)[0]
         topics = soup.find_all('div', {'class': 'topic'})
         category = {}
@@ -31,14 +36,20 @@ for year, num in tqdm(year_num):
 
         titles.append(title)
         tracks.append(track)
-        categories.append(category.get('Natural Language Processing', None))
+        if cat == 'nlp':
+            categories.append(category.get('Natural Language Processing', None))
+        elif cat == 'xai':
+            categories.append(category.get('AI Ethics, Trust, Fairness', None))
         urls.append(url)
+        descs.append(desc)
         
+    os.makedirs(f'./csv/{cat}/', exist_ok=True)
     pd.DataFrame(
         dict(
             title=titles,
             track=tracks,
             categories=categories,
-            url=urls,            
+            url=urls,
+            desc=descs
         )
-    ).to_csv(f'./csv/nlp/{year}_main.csv')
+    ).to_csv(f'./csv/{cat}/{year}_main.csv')
